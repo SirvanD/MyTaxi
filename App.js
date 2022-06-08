9acba0cb91b58630c355468f6b067fd5265442ab
SirvanD <sirvansh@gmail.com>
2022-06-08 15:56:27 +1000

    configured redux

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
 
 // 1) Set up redux
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Let's build MyTaxi</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <View style={styles.container}>
+        <Text>Let's build MyTaxi</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
