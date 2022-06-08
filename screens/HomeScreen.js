5b9d1802d4b777ace32b94b697e0a84a45796dbf
SirvanD <sirvansh@gmail.com>
2022-06-08 20:13:25 +1000

    implemented Google Places API

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,12 @@ import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -33,6 +37,7 @@ const HomeScreen = () => {
             // console.log(details);
           }}
           fetchDetails={true}
+          returnKeyType={"search"}
           enablePoweredByContainer={false}
           minLength={2}
           query={{ key: GOOGLE_MAPS_APIKEY, language: "en" }}
