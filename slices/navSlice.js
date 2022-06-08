a22dc098240a7c7e26b5966da0436155ecc42225
SirvanD <sirvansh@gmail.com>
2022-06-08 16:18:58 +1000

    configured redux

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -5,3 +5,22 @@ const initialState = {
   destination: null,
   travelTimeInformation: null,
 };
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
