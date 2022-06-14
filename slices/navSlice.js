4958dea6006d9a5fa99bb76831585d87bf6779df
SirvanD <sirvansh@gmail.com>
2022-06-14 14:20:17 +1000

    updated projects

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -6,7 +6,7 @@ const initialState = {
   travelTimeInformation: null,
 };
 
-//design slice for store
+//design slice and selectors for store
 export const navSlice = createSlice({
   name: "nav",
   initialState,
