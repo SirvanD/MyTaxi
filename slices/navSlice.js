7cdd44ab6d35451272af25b086f1fe9e655b4766
SirvanD <sirvansh@gmail.com>
2022-06-16 11:52:51 +1000

    updated Resume

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -6,7 +6,7 @@ const initialState = {
   travelTimeInformation: null,
 };
 
-//design slice and selectors for store
+//design slice and selectors for store and redux
 export const navSlice = createSlice({
   name: "nav",
   initialState,
